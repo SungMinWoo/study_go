b0e9cb9e615b9ecd787493c487fe6d2cc145cb0a
성민우 <minwoo@seongmin-uui-MacBookPro.local>
2024-04-21 23:58:18 +0900

    study return err

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,11 +1,31 @@
 package accounts
 
+import (
+	"errors"
+)
+
 type Account struct {
 	ower string
 	balance int
 }
 
+var errNoMoney error = errors.New("Can't withdraw you are poor")
+
 func NewAccount(ower string) *Account{
 	account := Account{ower: ower, balance: 0}
 	return &account
+}
+
+// Account의 method Account.Deposit()으로 호출 가능, a는 receiver
+func (a *Account) Deposit(amount int) {
+	a.balance += amount
+}
+
+// try-catch같은 예외처리가 존재하지 않아 따로 처리해줘야함, error가 없다면 nil을 반환해주면됨
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil
 }
